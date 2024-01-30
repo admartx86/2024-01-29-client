5bd39f58d9dfc87721f6f75183dc10e93e9d9bfa
Adam Martinez <12470793+mart3318@users.noreply.github.com>
2024-01-30 12:53:49 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2024-01-29-client
+
+A client for coding practice.
---
+++ script.test.js
@@ -0,0 +1,17 @@
+const { add, subtract, multiply, divide } = require("./script");
+
+test("adds 1 + 2 to equal 3", () => {
+    expect(add(1, 2)).toBe(3);
+});
+
+test("subtracts 1 - 2 to equal -1", () => {
+    expect(subtract(1, 2)).toBe(-1);
+});
+
+test("multiplies 1 * 2 to equal 2", () => {
+    expect(multiply(1, 2)).toBe(2);
+});
+
+test("divides 1 / 2 to equal 5", () => {
+    expect(divide(1, 2)).toBe(0.5);
+});
